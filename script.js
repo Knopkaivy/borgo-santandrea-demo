c710f06b8afd021fbf0882008ab219d5e7395496
TIVY-PK\tivy <tetiana.ivy@concentrix.com>
2025-03-13 09:16:09 -0500

    Add Beach section

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const variables = {
     dataSelectors: {
+        beachLink: '[data-beach-link]',
+        carouselLinkName: '[data-carousel-link-name]',
         languagesDropdown: '[data-languages-dropdown]',
         languagesAlternative: '[data-languages-alternative]',
         languagesChevron: '[data-languages-chevron]',
@@ -122,3 +124,91 @@ const handleElementTranslate = (element) =>{
     console.log(translateY);
     element.style.transform = `translateY(${translateY}%)`;
 }*/
+
+/* BEACH */
+const swiper = new Swiper('.swiper', {
+    speed: 1000,
+    slidesPerGroup: 1,
+    slidesPerView: 2.11,
+    centeredSlides: true,
+    initialSlide: 1,
+    grabCursor: true,
+    effect: "creative",
+    breakpoints: {
+    320: {
+    slidesPerView: 1.3,
+    creativeEffect: {
+    prev: {
+    shadow: false,
+    translate: ["-140%", 0, -700],
+    },
+    next: {
+    shadow: false,
+    translate: ["146%", 0, -700],
+    },
+    },
+    },
+    1280: {
+    slidesPerView: 2.11,
+    creativeEffect: {
+    prev: {
+    shadow: false,
+    translate: ["-140%", 0, -500],
+    },
+    next: {
+    shadow: false,
+    translate: ["146%", 0, -500],
+    },
+    },
+    }
+    },
+    navigation: {
+      nextEl: '.carousel-button-next',
+      prevEl: '.carousel-button-prev',
+    }
+  });
+
+  swiper.slideTo(1, false,false);
+
+const carouselLinkNameUpdateOnSlideChange = () =>{
+    setTimeout(()=>{
+        let name;
+    
+        switch (swiper.activeIndex){
+            case 0:
+                name = 'DOCKING AT THE BORGO';
+                break;
+            case 1:
+                name = 'MARINELLA PRIVATE BEACH';
+                break;
+            case 2:
+                name = 'BEACH CLUB PIZZERIA & GRILL';
+                break;
+            default:
+                break;
+        }
+    
+        document.querySelector(variables.dataSelectors.carouselLinkName).textContent = name;
+    },500)
+}
+
+const setLinkTransition = (dirA, dirB) =>{
+    const link = document.querySelector(variables.dataSelectors.beachLink);
+    
+const linkTransition = link.animate([{ transform: `translateX(0)`, opacity: 1 }, { transform: `translateX(${dirA}rem)`, opacity: .5 },{ transform: `translateX(0)`, opacity: 0 }, { transform: `translateX(${dirB/2}rem)`, opacity: .5 }, { transform: `translateX(0)`, opacity: 1 },],     {
+        fill: "forwards",
+        easing: "ease-in-out",
+        duration: 1000,
+        iterations: 1
+      },);
+    linkTransition.play();
+}
+
+swiper.on('slideChange', carouselLinkNameUpdateOnSlideChange);
+swiper.on('slideNextTransitionStart', function(){
+    setLinkTransition(-4, 4);
+});
+
+swiper.on('slidePrevTransitionStart', function(){
+    setLinkTransition(4, -4);
+});
